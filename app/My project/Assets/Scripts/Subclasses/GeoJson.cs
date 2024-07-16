54877349e30602fa31d8fa2cb120ffc9314f770f
Natalie Kim <ski102@uclive.ac.nz>
2024-07-16 15:32:26 +1200

    Maps integrated

--- app/My project/Assets/Scripts/Subclasses/GeoJson.cs
+++ app/My project/Assets/Scripts/Subclasses/GeoJson.cs
@@ -1,12 +1,23 @@
-﻿using UnityEditor;
-using UnityEngine;
-
-
-public class GeoJson
+﻿public class GeoJson
 {
     public string type { get; set; }
     public Geometry geometry { get; set; }
     public Properties properties { get; set; }
+
+    public GeoJson(string newType, Geometry geo, Properties prop)
+    {
+        type = newType;
+        geometry = geo;
+        properties = prop;
+    }
+
+    public GeoJson(Geometry geo, Properties prop)
+    {
+        geometry = geo;
+        properties = prop;
+    }
+
+    public GeoJson() { }
 } 
 
 public class Geometry
